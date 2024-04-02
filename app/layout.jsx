824436aecc82b31e62d4ad81a1df914209f3ce61
Yousae <yousaex24@gmail.com>
2024-04-02 12:34:39 -0500

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '@styles/global.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider'; 
+
+// meta data
+export const metadata = {
+    title: "Promptopia",
+    description: "Discover and Share AI Prompts"
+}
+
+const RootLayout = ({children}) => {
+    return (
+        <html lang="en">
+            <body>
+                <div className="main">
+                    <div className="gradient"/>
+                </div>
+                <main className="app">
+                    <Nav/>
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout;
